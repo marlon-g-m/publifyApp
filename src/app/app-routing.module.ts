868e610e71b8969744e986f3da85bdb6cbd8d15d
marlon-g-m <marlon.r.meza@gmail.com>
2021-11-12 02:35:06 -0300

    antes  de amplify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'login'},
+  {path:'login',component : LoginComponent},
+  {path:'sign-up',component : SignUpComponent},
+  {path:'home',component : HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
